e789188d10416e0cd928ce07a0889366ea9e38ee
Narkolapin <narkolapin@gmail.com>
2015-03-07 14:49:32 +0100

    Netoyage de l'application

--- app.rb
+++ app.rb
@@ -5,5 +5,12 @@ module ChatDemo
     get "/" do
       erb :"index.html"
     end
+
+    get "public/assets/js/application.js" do
+		content_type :js
+		@scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
+		erb :"application.js"
+	end
+
   end
 end
