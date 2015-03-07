a67d000f0a401a880435e91fdc6155c39d22d8a6
Narkolapin <narkolapin@gmail.com>
2015-03-07 16:52:15 +0100

    ajout d'une page d'auth et des session dans app.rb

--- app.rb
+++ app.rb
@@ -2,8 +2,23 @@ require 'sinatra/base'
 
 module ChatDemo
   class App < Sinatra::Base
+
+    set :sessions, true
+
     get "/" do
-      erb :"index.html"
+      erb :"login.html"
+    end
+
+    post '/login' do
+      session['utilisateur'] = params['user']
+      session['motdepass'] = params['pwd']
+      response.set_cookie(session['utilisateur'],settings.token) 
+      redirect '/'
+    end
+
+    get "/chat" do
+      user = session['utilisateur']
+      erb :"chat.html"
     end
   end
 end
