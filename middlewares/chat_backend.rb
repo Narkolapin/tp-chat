89c52a4d45b1da39303a3decb312d5f68ced17d9
Narkolapin <narkolapin@gmail.com>
2015-03-10 00:12:06 +0100

    test bdd

--- middlewares/chat_backend.rb
+++ middlewares/chat_backend.rb
@@ -39,7 +39,7 @@ module TpChat
 			#message
 			ws.on :message do |event|
 				p [:message, event.data]
-				#@coll.insert([{"message" => event.data}])
+				@coll.insert([{:content => :message}])
 				@clients.each {|client| client.send(event.data) }
 				
 			end
